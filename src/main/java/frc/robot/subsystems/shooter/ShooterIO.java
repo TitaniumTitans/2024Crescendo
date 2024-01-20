4946ff67e8737f26f28a7240c6b5b8653ec0f010
Steggo 4467 <114831754+Steggoo@users.noreply.github.com>
2024-01-20 10:28:33 -0500

    Feat: Added ShooterIOSim class

--- src/main/java/frc/robot/subsystems/shooter/ShooterIO.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterIO.java
@@ -6,8 +6,20 @@ public interface ShooterIO {
 
     @AutoLog
     public class ShooterIOInputs {
+        public double tLAngularVelocity = 0.0;
+        public double tRAngularVelocity = 0.0;
+        public double bLAngularVelocity = 0.0;
+        public double bRAngularVelocity = 0.0;
+        public double kickerAngularVelocity = 0.0;
 
+        public double tLAppliedInputs = 0.0;
+        public double tRAppliedInputs = 0.0;
+        public double bLAppliedInputs = 0.0;
+        public double bRAppliedInputs = 0.0;
+        public double kickerAppliedInputs = 0.0;
     }
+
+    public default void updateInputs(){}
     default void setMotorVoltageTL(double voltage) {}
     default void setMotorVoltageTR(double voltage) {}
     default void setMotorVoltageBL(double voltage) {}
---
+++ src/main/java/frc/robot/subsystems/shooter/ShooterIOSim.java
@@ -0,0 +1,68 @@
+package frc.robot.subsystems.shooter;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.wpilibj.simulation.DCMotorSim;
+import edu.wpi.first.wpilibj.simulation.FlywheelSim;
+
+public class ShooterIOSim implements ShooterIO{
+    private final FlywheelSim m_simTL = new FlywheelSim(
+            DCMotor.getNeoVortex(1), 1, 1);
+    private final FlywheelSim m_simTR = new FlywheelSim(
+            DCMotor.getNeoVortex(1), 1, 1);
+    private final FlywheelSim m_simBL = new FlywheelSim(
+            DCMotor.getNeoVortex(1), 1, 1);
+    private final FlywheelSim m_simBR = new FlywheelSim(
+            DCMotor.getNeoVortex(1), 1, 1);
+    private final FlywheelSim m_simKicker = new FlywheelSim(
+            DCMotor.getNeoVortex(1),1,1);
+
+    private double tLAppliedVolts = 0.0;
+    private double tRAppliedVolts = 0.0;
+    private double bLAppliedVolts = 0.0;
+    private double bRAppliedVolts = 0.0;
+    private double kickerAppliedVolts = 0.0;
+
+    public void updateInputs(ShooterIOInputs inputs) {
+        inputs.tLAngularVelocity = m_simTL.getAngularVelocityRadPerSec();
+        inputs.tRAngularVelocity = m_simTR.getAngularVelocityRadPerSec();
+        inputs.bLAngularVelocity = m_simBL.getAngularVelocityRadPerSec();
+        inputs.bRAngularVelocity = m_simBR.getAngularVelocityRadPerSec();
+        inputs.kickerAngularVelocity = m_simKicker.getAngularVelocityRadPerSec();
+
+        inputs.tLAppliedInputs = tLAppliedVolts;
+        inputs.tRAppliedInputs = tRAppliedVolts;
+        inputs.bLAppliedInputs = bLAppliedVolts;
+        inputs.bRAppliedInputs = bRAppliedVolts;
+        inputs.kickerAppliedInputs = kickerAppliedVolts;
+    }
+    @Override
+    public void setMotorVoltageTL(double voltage) {
+        tLAppliedVolts = MathUtil.clamp(voltage, -12.0, 12.0);
+        m_simTL.setInputVoltage(voltage);
+    }
+
+    @Override
+    public void setMotorVoltageTR(double voltage) {
+        tRAppliedVolts = MathUtil.clamp(voltage, -12.0, 12.0);
+        m_simTR.setInputVoltage(voltage);
+    }
+
+    @Override
+    public void setMotorVoltageBL(double voltage) {
+        bLAppliedVolts = MathUtil.clamp(voltage, -12.0, 12.0);
+        m_simBL.setInputVoltage(voltage);
+    }
+
+    @Override
+    public void setMotorVoltageBR(double voltage) {
+        bRAppliedVolts = MathUtil.clamp(voltage, -12.0, 12.0);
+        m_simBR.setInputVoltage(voltage);
+    }
+
+    @Override
+    public void setKickerVoltage(double voltage) {
+        kickerAppliedVolts = MathUtil.clamp(voltage,-12.0,12.0);
+        m_simKicker.setInputVoltage(voltage);
+    }
+}
