cefe6cc0bd3cce3a39b9333d01dcf6decb6e774a
Steggo 4467 <114831754+Steggoo@users.noreply.github.com>
2024-01-25 20:01:57 -0500

    Feat: Added Subsystem

---
+++ src/main/java/frc/robot/subsystems/vision/VisionSubsystem.java
@@ -0,0 +1,14 @@
+package frc.robot.subsystems.vision;
+
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class VisionSubsystem extends SubsystemBase {
+    public VisionSubsystem() {
+        // TODO: Set the default command, if any, for this subsystem by calling setDefaultCommand(command)
+        //       in the constructor or in the robot coordination class, such as RobotContainer.
+        //       Also, you can call addChild(name, sendableChild) to associate sendables with the subsystem
+        //       such as SpeedControllers, Encoders, DigitalInputs, etc.
+    }
+}
+
