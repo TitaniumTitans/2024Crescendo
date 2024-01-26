65b97adc4cf3ce51bead53dffd79ec1d1003df71
ItchyMoose281 <robbymnick@gmail.com>
2024-01-25 20:40:18 -0500

    Update VisionSubsystem.java
    Feat: Created constructor for PhotonVision Subsystem

--- src/main/java/frc/robot/subsystems/vision/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/vision/VisionSubsystem.java
@@ -1,14 +1,46 @@
 package frc.robot.subsystems.vision;
 
 
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonPoseEstimator;
+
+import java.io.IOException;
 
 public class VisionSubsystem extends SubsystemBase {
-    public VisionSubsystem() {
-        // TODO: Set the default command, if any, for this subsystem by calling setDefaultCommand(command)
-        //       in the constructor or in the robot coordination class, such as RobotContainer.
-        //       Also, you can call addChild(name, sendableChild) to associate sendables with the subsystem
-        //       such as SpeedControllers, Encoders, DigitalInputs, etc.
+     private final PhotonCamera m_camera;
+     private final Transform3d robotToCam;
+     private PhotonPoseEstimator m_photonPoseEstimator;
+     private final DriverStation.Alliance m_alliance;
+     private AprilTagFieldLayout m_aprilTagFieldLayout;
+    public VisionSubsystem(String camName, Transform3d camPose) {
+        m_camera = new PhotonCamera(camName);
+        robotToCam = camPose;
+        if(DriverStation.getAlliance()!=null) {
+            m_alliance = DriverStation.getAlliance().get();
+        } else {
+            m_alliance = DriverStation.Alliance.Blue;
+        }
+        try {
+            m_aprilTagFieldLayout =AprilTagFieldLayout.loadFromResource(AprilTagFields.k2024Crescendo.m_resourceFile);
+            if(m_alliance == DriverStation.Alliance.Blue){
+                m_aprilTagFieldLayout.setOrigin(AprilTagFieldLayout.OriginPosition.kBlueAllianceWallRightSide);
+            } else {
+                m_aprilTagFieldLayout.setOrigin(AprilTagFieldLayout.OriginPosition.kRedAllianceWallRightSide);
+            }
+            m_photonPoseEstimator = new PhotonPoseEstimator(
+                    m_aprilTagFieldLayout,
+                    PhotonPoseEstimator.PoseStrategy.CLOSEST_TO_REFERENCE_POSE,
+                    m_camera,
+                    robotToCam);
+        } catch (IOException e){
+            throw new IllegalStateException(e);
+        }
+
     }
 }
 
