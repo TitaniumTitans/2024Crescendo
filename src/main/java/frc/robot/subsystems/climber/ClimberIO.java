ac8b1ac1dcdb50c59a61c401e5e13bcedfddd721
superdf407 <114831754+Steggoo@users.noreply.github.com>
2024-01-31 20:36:37 -0500

    feat: Climber IO Inputs Logged Values

--- src/main/java/frc/robot/subsystems/climber/ClimberIO.java
+++ src/main/java/frc/robot/subsystems/climber/ClimberIO.java
@@ -1,6 +1,24 @@
 package frc.robot.subsystems.climber;
 
+import com.ctre.phoenix6.hardware.TalonFX;
+import edu.wpi.first.math.geometry.Rotation2d;
+import org.littletonrobotics.junction.AutoLog;
+
 public interface ClimberIO {
+    @AutoLog
+    class ClimberIOInputs {
+        public double leftClimberPosition = 0.0;
+        public double rightClimberPosition = 0.0;
+        public double leftClimberVelocity = 0.0;
+        public double rightClimberVelocity = 0.0;
+        public double leftClimberCurrentDraw = 0.0;
+        public double rightClimberCurrentDraw = 0.0;
+        public double leftClimberCurrentSetpoint = 0.0;
+        public double rightClimberCurrentSetpoint = 0.0;
+        public double leftClimberAppliedOutput = 0.0;
+        public double rightClimberAppliedOutput = 0.0;
+
+    }
 
     default void setRightPosition(double degrees) {}
     default void setLeftPosition(double degrees) {}
