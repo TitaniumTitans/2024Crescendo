bf31570fa57d3888cb83073a8e31e0022f866cc3
Logan Hollowood <98189655+GearBoxFox@users.noreply.github.com>
2024-02-03 11:23:29 -0500

    optimized imports

--- src/main/java/frc/robot/subsystems/drive/module/Module.java
+++ src/main/java/frc/robot/subsystems/drive/module/Module.java
@@ -13,8 +13,6 @@
 
 package frc.robot.subsystems.drive.module;
 
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
