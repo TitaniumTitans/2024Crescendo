e9cec3be99607575c9bf43bce124986e16fd8100
Logan Hollowood <98189655+GearBoxFox@users.noreply.github.com>
2024-03-15 20:39:08 -0400

    track width is different?

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -73,7 +73,7 @@ public final class Constants {
 
     public static final double MAX_LINEAR_SPEED = Units.feetToMeters(17.8);
     public static final double TRACK_WIDTH_X = Units.inchesToMeters(18.6);
-    public static final double TRACK_WIDTH_Y = Units.inchesToMeters(18.6);
+    public static final double TRACK_WIDTH_Y = Units.inchesToMeters(20.6);
     public static final double DRIVE_BASE_RADIUS =
         Math.hypot(TRACK_WIDTH_X / 2.0, TRACK_WIDTH_Y / 2.0);
     public static final double MAX_ANGULAR_SPEED = MAX_LINEAR_SPEED / DRIVE_BASE_RADIUS;
